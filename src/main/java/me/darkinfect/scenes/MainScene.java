2603c06c6242164e15989d37e0e954462412cb95
Kirill Kuharev <vectordifenstyeeder@gmail.com>
2025-07-08 12:13:15 +0300

    v0.0.6

--- src/main/java/me/darkinfect/scenes/MainScene.java
+++ src/main/java/me/darkinfect/scenes/MainScene.java
@@ -12,22 +12,24 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.utils.Align;
 
 public class MainScene implements Screen {
     private Skin skin;
     private SpriteBatch batch;
     private Texture backgroundTexture;
     private Label BossFigth;
-    private Stage stage = new Stage();
+    private final Stage stage = new Stage();
     private ImageButton coinButton;
     private Label coinLabel;
     private int Coins = 0;
+    private ImageButton menuButton;
+    private Table menu;
+    private boolean isMenuVisible = false;
 
     @Override
     public void show() {
@@ -35,13 +37,14 @@ public class MainScene implements Screen {
         batch = new SpriteBatch();
         skin = new Skin();
         initbutton();
+        initMenu();
+
         Label.LabelStyle labelStyle = new Label.LabelStyle();
-        labelStyle.font = new BitmapFont(); // Можно заменить на свой шрифт
+        labelStyle.font = new BitmapFont();
         labelStyle.fontColor = Color.GOLD;
 
-        // Инициализируем Label
         coinLabel = new Label("Coins: " + Coins, labelStyle);
-        coinLabel.setPosition(20, Gdx.graphics.getHeight() - 50); // Позиция в верхнем левом углу
+        coinLabel.setPosition(Gdx.graphics.getWidth()-100, Gdx.graphics.getHeight() - 50);
 
         stage.addActor(coinLabel);
         Gdx.input.setInputProcessor(stage);
@@ -57,7 +60,6 @@ public class MainScene implements Screen {
         batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.end();
 
-        // Отрисовка сцены с кнопкой
         stage.act(delta);
         stage.draw();
     }
@@ -81,20 +83,66 @@ public class MainScene implements Screen {
         batch.dispose();
         backgroundTexture.dispose();
         stage.dispose();
-        skin.dispose(); // Не забываем освободить skin
+        skin.dispose();
+    }
+    private void initMenu(){
+        skin= new Skin(Gdx.files.internal("uiskin.json"));
+        Texture buttonTexture = new Texture(Gdx.files.internal("menu.jpg"));
+        ImageButton.ImageButtonStyle imageButtonStyle = new ImageButton.ImageButtonStyle();
+        imageButtonStyle.imageUp = new TextureRegionDrawable(new TextureRegion(buttonTexture));
+        skin.add("menuButtonStyle", imageButtonStyle);
+        menuButton = new ImageButton(skin,"menuButtonStyle");
+        menuButton.setPosition(20, Gdx.graphics.getHeight() - 70);
+        menuButton.setSize(50, 50);
+
+        // Создание выпадающего меню
+        menu = new Table(skin);
+        menu.setBackground(new TextureRegionDrawable(new TextureRegion(createWhitePixel())));
+        menu.align(Align.topLeft);
+        menu.pad(10);
+        menu.setPosition(20, Gdx.graphics.getHeight() - 150);
+        menu.setVisible(false);
+
+
+        TextButton button1 = new TextButton("New Game", skin);
+        TextButton button2 = new TextButton("Settings", skin);
+        TextButton button3 = new TextButton("Exit", skin);
+
+        menu.add(button1).padBottom(10).width(200).height(50).row();
+        menu.add(button2).padBottom(10).width(200).height(50).row();
+        menu.add(button3).width(200).height(50);
+
+        // Обработчик клика по кнопке "гамбургер"
+        menuButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                isMenuVisible = !isMenuVisible;
+                menu.setVisible(isMenuVisible);
+            }
+        });
+
+        // Обработчики для кнопок меню
+        button1.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("New Game clicked!");
+            }
+        });
+
+        button3.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        stage.addActor(menuButton);
+        stage.addActor(menu);
+
     }
     private void initbutton(){
-        Pixmap pixmap = new Pixmap(Gdx.files.internal("coin.png"));
-        Format format = pixmap.getFormat();
-        if (format != Pixmap.Format.RGBA8888) {
-            Pixmap newPixmap = new Pixmap(pixmap.getWidth(), pixmap.getHeight(), Pixmap.Format.RGBA8888);
-            newPixmap.drawPixmap(pixmap, 0, 0);
-            pixmap = newPixmap;
-        }
-        Texture coinTexture = new Texture(Gdx.files.internal("coin.png"),true);
-        coinTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        Texture coinTexture = new Texture(Gdx.files.internal("clickbutton.jpg"));
         skin.add("coin", coinTexture);
-        pixmap.dispose();
 
         ImageButton.ImageButtonStyle coinButtonStyle = new ImageButton.ImageButtonStyle();
         coinButtonStyle.imageUp = new TextureRegionDrawable(new TextureRegion(skin.get("coin", Texture.class)));
@@ -122,8 +170,15 @@ public class MainScene implements Screen {
 
         stage.addActor(coinButton);
     }
-    // Функция для обновления счётчика
     public void updateCoinCounter() {
         coinLabel.setText("Coins: " + Coins);
+    }
+    private Texture createWhitePixel() {
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        Texture texture = new Texture(pixmap);
+        pixmap.dispose();
+        return texture;
     }
 }
